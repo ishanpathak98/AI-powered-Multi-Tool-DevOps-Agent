5ae71f42f093570171fd6ad8178791ca32033747
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2025-07-11 01:01:54 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,65 +1,56 @@
+# AWS Settings
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "The AWS region to deploy resources"
   type        = string
-  default     = "ap-south-1"
-}
-
-variable "environment" {
-  description = "Deployment environment"
-  type        = string
-  default     = "dev"
+  default     = "us-east-1"
 }
 
 variable "project_name" {
-  description = "Project identifier prefix"
+  description = "Name of the project"
   type        = string
-  default     = "devops-ai-agent"
+  default     = "devops-agent"
 }
 
+variable "key_pair_name" {
+  description = "Name of the AWS key pair for SSH access"
+  type        = string
+}
+
+# VPC & Subnets
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+# Instance Configuration
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
 }
 
-variable "vpc_cidr" {
-  description = "VPC CIDR block"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  description = "List of public subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  description = "List of private subnet CIDRs"
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
-}
-
+# Monitoring Agent Versions
 variable "node_exporter_version" {
-  description = "Version of Prometheus Node Exporter to install"
+  description = "Version of Prometheus Node Exporter"
   type        = string
-  default     = "1.8.1"
+  default     = "1.7.0"
 }
 
 variable "promtail_version" {
-  description = "Version of Promtail to install"
+  description = "Version of Promtail agent"
   type        = string
-  default     = "2.9.4"
-}
-
-variable "grafana_password" {
-  description = "Grafana admin password"
-  type        = string
-  default     = "admin123"
-}
-
-variable "key_pair_name" {
-  description = "Name of the AWS key pair to use for EC2 access"
-  type        = string
-  default     = "devops-ai-agent-key"
+  default     = "2.9.3"
 }
