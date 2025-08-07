7fac4399f9f63200cc1da6f3125817f31fa9ed28
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2025-08-08 00:03:17 +0530

    Create ec2.tf
    Created ec2. fixed some syntax errors

---
+++ Terraform/ec2.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create Security Group
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_sg"
+  description = "Allow access to Jenkins on port 8080"
+
+  ingress {
+    description = "Allow Jenkins HTTP UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins_sg"
+  }
+}
